2fbe7b2a60c9a9f6d97d2634b7c494c2c1a2615d
carolluckmann <carolinaluckmann05@gmail.com>
2024-03-11 15:07:03 -0300

    Criação da API de Categoria

--- app/urls.py
+++ app/urls.py
@@ -9,16 +9,10 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
-
-
-from core.views import UserViewSet
-from core.views import CategoriaViewSet # nova linha
+from core.views import UserViewSet, CategoriaViewSet
 
 router = DefaultRouter()
-router.register(r"categorias", CategoriaViewSet) # nova linha
-router.register(r"users", UserViewSet, basename="users")
-
+router.register(r"categorias", CategoriaViewSet, basename="categorias")
 router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
