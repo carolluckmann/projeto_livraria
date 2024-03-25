36dba4a0efbb7613ebb29eb96f4e4143da7b7ba3
carolluckmann <carolinaluckmann05@gmail.com>
2024-03-25 10:56:50 -0300

    criação da API para Autor

--- core/models/editora.py
+++ core/models/editora.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
-    site = models.URLField(max_length=200, blank=True, null=True)
+    site = models.URLField(max_length = 100, blank=True, null=True)
 
     def __str__(self):
-        return self.nome
+        return {self.nome}
